7ff891bff586b50af3444d3bdcfcb2819079b3b6
Vatsal <327178@viauc.dk>
2023-04-07 14:59:49 +0200

    Sservices Done

--- Application/Services/AuthService.cs
+++ Application/Services/AuthService.cs
@@ -1,19 +1,54 @@
-﻿using Application.Shared.Model;
+﻿using System.ComponentModel.DataAnnotations;
+using Application.Shared.DTO;
+using Application.Shared.Model;
 
 namespace Application.Services;
 
 public class AuthService : IAuthService
 {
-    private IList<User> Users = new List<User>();
-
-
+    private IList<User> Users;
     public Task<User> GetUser(string username, string password)
     {
-        throw new NotImplementedException();
+        User? existingUser = Users.FirstOrDefault(u => 
+            u.Username.Equals(username, StringComparison.OrdinalIgnoreCase));
+        
+        if (existingUser == null)
+        {
+            throw new Exception("User not found");
+        }
+
+        if (!existingUser.Password.Equals(password))
+        {
+            throw new Exception("Password mismatch");
+        }
+
+        return Task.FromResult(existingUser);
     }
 
-    public Task RegisterUser(User user)
+    public Task RegisterUser(RegisterDTO userRegisterDto)
     {
-        throw new NotImplementedException();
+        if (string.IsNullOrEmpty(userRegisterDto.UserName))
+        {
+            throw new ValidationException("Username cannot be null");
+        }
+
+        if (string.IsNullOrEmpty(userRegisterDto.Password))
+        {
+            throw new ValidationException("Password cannot be null");
+        }
+
+        var newUser = new User
+        {
+            Username = userRegisterDto.UserName,
+            Password = userRegisterDto.Password,
+            Age = userRegisterDto.Age,
+            Email = userRegisterDto.Email,
+            Name = userRegisterDto.Name
+        };
+        
+        Users.Add(newUser);
+        
+        return Task.CompletedTask;
     }
+    
 }
--- Application/Services/IAuthService.cs
+++ Application/Services/IAuthService.cs
@@ -1,10 +1,11 @@
-﻿using Application.Shared.Model;
+﻿using Application.Shared.DTO;
+using Application.Shared.Model;
 
 namespace Application.Services;
 
 public interface IAuthService
 {
     Task<User> GetUser(string username, string password);
-    Task RegisterUser(User user);
+    Task RegisterUser(RegisterDTO userRegisterDto);
     
 }
